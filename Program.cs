788d4bf967c35a06ab9bd2ef425954c4d4d9891e
kongo2002 <kongo2002@googlemail.com>
2014-03-20 21:34:29 +0100

    add example ServiceStack usage

--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
 using System;
+using System.Reflection;
 
 using Mono.Unix;
 using Mono.Unix.Native;
 
+using Funq;
+
+using ServiceStack;
+
 namespace monotest
 {
-    class MainClass
+    public class AppHost : AppHostHttpListenerBase
+    {
+        public AppHost()
+            : base("monotest", Assembly.GetExecutingAssembly())
+        { }
+
+        public override void Configure (Container container)
+        { }
+    }
+
+    class MonoTest
     {
         public static void Main (string[] args)
         {
-            var exit = false;
-            
             Console.WriteLine ("Starting test deamon...");
-            
+
+            var exit = false;
             var signals = new[] {
                 new UnixSignal(Signum.SIGINT),
                 new UnixSignal(Signum.SIGTERM)
             };
-            
-            
+
+            var host = new AppHost();
+
+            host.Init();
+            host.Start("http://+:8080/");
+
             // wait for termination
             while (!exit)
             {
                 var id = UnixSignal.WaitAny(signals);
-                
+
                 if (id >= 0 && id < signals.Length)
                 {
                     if (signals[id].IsSet)
